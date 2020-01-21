9b087d6843a985f1ab0693068a2698bdb3f5f416
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-01-21 12:14:49 +0200

    Updates

---
+++ precache-manifest.ca5ea09e40e93edbf3a486a102e08948.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a637c5bdf8c040f1ac4c17b46ef12e12",
+    "url": "/Skrini_sosial_network/index.html"
+  },
+  {
+    "revision": "a2d6bf09382a974cfbd0",
+    "url": "/Skrini_sosial_network/static/css/3.ebb08a3b.chunk.css"
+  },
+  {
+    "revision": "2b243e86e6220d007d05",
+    "url": "/Skrini_sosial_network/static/css/4.c204166f.chunk.css"
+  },
+  {
+    "revision": "ba5a1cd7686f9121e729",
+    "url": "/Skrini_sosial_network/static/css/main.34e005d9.chunk.css"
+  },
+  {
+    "revision": "7c71c7a15ad030ec2166",
+    "url": "/Skrini_sosial_network/static/js/2.e13bb2b6.chunk.js"
+  },
+  {
+    "revision": "690056fc8e01b0a186df66452a50918a",
+    "url": "/Skrini_sosial_network/static/js/2.e13bb2b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2d6bf09382a974cfbd0",
+    "url": "/Skrini_sosial_network/static/js/3.aee62dec.chunk.js"
+  },
+  {
+    "revision": "2b243e86e6220d007d05",
+    "url": "/Skrini_sosial_network/static/js/4.1315fbbc.chunk.js"
+  },
+  {
+    "revision": "ba5a1cd7686f9121e729",
+    "url": "/Skrini_sosial_network/static/js/main.6b68d386.chunk.js"
+  },
+  {
+    "revision": "9afa958b661edef243d2",
+    "url": "/Skrini_sosial_network/static/js/runtime-main.e70bc669.js"
+  },
+  {
+    "revision": "a1d40c3876bd1460ff1e9e3858d699c7",
+    "url": "/Skrini_sosial_network/static/media/images.a1d40c38.png"
+  },
+  {
+    "revision": "3eb3f5ee88995667819bafdcb9df7f85",
+    "url": "/Skrini_sosial_network/static/media/loading.3eb3f5ee.svg"
+  }
+]);
